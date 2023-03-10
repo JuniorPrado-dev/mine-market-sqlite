28b5944d038d3a0f8b840f62e6a26f6e6216d072
Junior Prado <juniorcvnnn@gmail.com>
2023-03-10 17:31:40 -0300

    add endereço

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -4,9 +4,15 @@ import { productRouter } from './router/productRouter';
 import { orderRouter } from './router/orderRouter';
 import { Request, Response } from 'express';
 
+import path  from 'path';
+
+const local=path.resolve("../src")
+console.log(local);
+
 app.get("/",(req:Request,resp:Response)=>{
-    resp.send("API Mine Markert ok!")
+    resp.send(`API Mine Markert ok! ${local}`)
 })
 app.use("/client",clientRouter);
 app.use("/product",productRouter);
 app.use("/order",orderRouter);
+
